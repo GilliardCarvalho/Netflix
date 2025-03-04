191ece9584421103291fe48f02ee90dc002f309a
GilliardCarvalho <gilliard.carvalho6@gmail.com>
2025-03-04 20:45:32 -0300

    alterações carrossel

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 5,
-    spaceBetween: 10,
-    loop: true,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    breakpoints: {
-        1024: { slidesPerView: 5 },
-        768: { slidesPerView: 3 },
-        480: { slidesPerView: 2 }
-    }
-});
+<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+<script>
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 5, // Quantidade de slides visíveis
+        spaceBetween: 10, // Espaço entre os slides
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        breakpoints: {
+            // Responsividade
+            320: {
+                slidesPerView: 2,
+                spaceBetween: 10,
+            },
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+            },
+            1024: {
+                slidesPerView: 5,
+                spaceBetween: 30,
+            },
+        },
+    });
+</script>
